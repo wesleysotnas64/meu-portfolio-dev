fdeef81ef0215a541d1fa3672fb15ea5a5b68999
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-06-17 16:26:57 -0300

    feat: Add Skane ProjectCard at ProjectGridGames;

--- src/components/projectGridGames/ProjectGridGames.jsx
+++ src/components/projectGridGames/ProjectGridGames.jsx
@@ -1,6 +1,7 @@
 import ProjectCard from "../projectCard/ProjectCard";
 import { GridContainer } from "./ProjectGridGames.style";
 import pongImage from "../../assets/img/pong.jpg";
+import snakeImage from "../../assets/img/snake.jpg";
 
 function ProjectGridGames() {
     return (
@@ -10,6 +11,12 @@ function ProjectGridGames() {
                 description="Clone do clássico jogo dos anos 80, Pong. Desenvolvido na Unity e outras ferramentas de desenvolvimento de jogos"
                 backgroundImage={pongImage}
                 projectLink="https://wesleysotnas64.itch.io/pong-clone-portfolio"
+            />
+            <ProjectCard
+                title="Purple Snake"
+                description="Uma versão minimalista do clássico Snake. Desenvolvido na Unity e outras ferramentas de desenvolvimento de jogos"
+                backgroundImage={snakeImage}
+                projectLink="https://wesleysotnas64.itch.io/purple-snake"
             />
             
         </GridContainer>
